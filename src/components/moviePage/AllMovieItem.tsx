3743f639211cc5d61b5d84525f4f297906f082dc
Amir <najiamirdesktop@gmail.com>
2024-05-11 12:47:32 +0330

    update all movies page

--- src/components/moviePage/AllMovieItem.tsx
+++ src/components/moviePage/AllMovieItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Movie } from "../../models/Movie";
 import { ImageUrl } from "../../requestConfig";
 
@@ -7,14 +8,16 @@ type AllMovieItemProps = {
 
 function AllMovieItem({ movie }: AllMovieItemProps) {
   return (
-    <div className="flex flex-col flex-shrink-0 gap-2 items-center w-28 h-full">
-      <img
-        src={ImageUrl + movie.poster_path}
-        alt="movie poster"
-        className="rounded-lg"
-      />
-      <div className="text-sm text-gray-600 font-semibold">{movie.title}</div>
-    </div>
+    <Link to={'../movie-details'} state={movie}>
+      <div className="flex flex-col flex-shrink-0 gap-2 items-center w-28 h-full">
+        <img
+          src={ImageUrl + movie.poster_path}
+          alt="movie poster"
+          className="rounded-lg"
+        />
+        <div className="text-sm text-gray-600 font-semibold">{movie.title}</div>
+      </div>
+    </Link>
   );
 }
 
