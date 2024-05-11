9a1793caddfa9abe1c12ba02bdb0aa8be0789dbc
Amir <najiamirdesktop@gmail.com>
2024-05-11 13:02:55 +0330

    update all tv page

--- src/components/moviePage/AllSeriesItem.tsx
+++ src/components/moviePage/AllSeriesItem.tsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
 import { Tv } from "../../models/Tv";
 import { ImageUrl } from "../../requestConfig";
 
 type AllSeriesItemProps = {
-    tv: Tv
-}
+  tv: Tv;
+};
 
-
-function AllSeriesItem({tv}: AllSeriesItemProps) {
+function AllSeriesItem({ tv }: AllSeriesItemProps) {
   return (
-    <div className="flex flex-col flex-shrink-0 gap-2 items-center w-28 h-full">
-      <img
-        src={ImageUrl + tv.poster_path}
-        alt="movie poster"
-        className="rounded-lg"
-      />
-      <div className="text-sm text-gray-600 font-semibold">{tv.name}</div>
-    </div>
+    <Link to={"../tv-details"} state={tv}>
+      <div className="flex flex-col flex-shrink-0 gap-2 items-center w-28 h-full">
+        <img
+          src={ImageUrl + tv.poster_path}
+          alt="movie poster"
+          className="rounded-lg"
+        />
+        <div className="text-sm text-gray-600 font-semibold">{tv.name}</div>
+      </div>
+    </Link>
   );
 }
 
