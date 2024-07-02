75778e53ee4e4295f1302513780d8de8a9061d6a
Amir <najiamirdesktop@gmail.com>
2024-07-02 18:59:32 +0330

    update genre page

--- src/components/genres/GenreItem.tsx
+++ src/components/genres/GenreItem.tsx
@@ -14,7 +14,7 @@ function GenreItem({ genre, color, title }: GenreItemProps) {
       title
     }}>
       <div
-        className={`flex w-44 h-28 rounded-lg p-5 ${color} items-center justify-center`}
+        className={`flex w-40 h-28 rounded-lg p-5 ${color} items-center justify-center`}
       >
         <span className="text-xl text-white font-main-font">{genre.name}</span>
       </div>
