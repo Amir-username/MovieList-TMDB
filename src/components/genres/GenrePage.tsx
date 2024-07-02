e78a5d55f942c09aa9c4e2dd71f7413099f8f464
Amir <najiamirdesktop@gmail.com>
2024-07-02 18:10:54 +0330

    added pagination to genre page

--- src/components/genres/GenrePage.tsx
+++ src/components/genres/GenrePage.tsx
@@ -5,29 +5,32 @@ import { BaseURL, options } from "../../requestConfig";
 import { Movie } from "../../models/Movie";
 import AllMoviesContent from "../moviePage/AllMoviesContent";
 import MoviesBrand from "../brand/MoviesBrand";
+import Pagination from "../pagination/Pagination";
 
 function GenrePage() {
   const location = useLocation();
   const {genre, title} = location.state;
 
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [moviePage, setMoviePage] = useState<number>(1);
 
   useEffect(() => {
     axios
       .get(
         BaseURL +
-          `/discover/${title}?page=1&sort_by=popularity.desc&with_genres=${genre.id}`,
+          `/discover/${title}?page=${moviePage}&sort_by=popularity.desc&with_genres=${genre.id}`,
         options
       )
       .then((res) => {
         setMovies(res.data.results);
       });
-  }, []);
+  }, [moviePage]);
 
   return (
-    <div className="flex flex-col m-5 gap-8">
+    <div className="flex flex-col m-5 gap-8 mb-32">
       <MoviesBrand name={genre.name} />
       <AllMoviesContent movies={movies} />
+      <Pagination pageNum={moviePage} setPageNum={setMoviePage} />
     </div>
   );
 }
