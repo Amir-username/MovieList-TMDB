3ddd788f922bfee91e1c12cd8374bfb71180cbdc
Amir <najiamirdesktop@gmail.com>
2024-07-11 19:37:11 +0330

    added cancel button for search box

--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -10,15 +10,23 @@ type SearchBoxProps = {
   setPersonResult: React.Dispatch<React.SetStateAction<Credit[]>>;
 };
 
-function SearchBox({ setMovieResult, setTvResult, setPersonResult }: SearchBoxProps) {
+function SearchBox({
+  setMovieResult,
+  setTvResult,
+  setPersonResult,
+}: SearchBoxProps) {
   const [search, setSearch] = useState<string>("");
 
-  useSearch(search, 'movie', setMovieResult)
-  useSearch(search, 'tv', setTvResult)
-  useSearch(search, 'person', setPersonResult)
+  useSearch(search, "movie", setMovieResult);
+  useSearch(search, "tv", setTvResult);
+  useSearch(search, "person", setPersonResult);
 
   const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+  };
+
+  const onCancel = () => {
+    setSearch("");
   };
 
   return (
@@ -30,11 +38,22 @@ function SearchBox({ setMovieResult, setTvResult, setPersonResult }: SearchBoxPr
       </div>
       <input
         onChange={onSearch}
+        value={search}
         className="font-main-font text-lg dark:bg-gray-900 placeholder:text-gray-400 pl-10 text-gray-600 dark:text-gray-300 ring-1
          focus:outline-none border-0 focus:ring-2 focus:ring-primary-dark
          ring-gray-300 dark:ring-primary-dark p-4 rounded-full w-full"
         placeholder="search movies"
       />
+      {search && (
+        <div className="relative">
+          <span
+            onClick={onCancel}
+            className="text-xl cursor-pointer top-0.5 absolute inset-y-0 end-5 flex items-center ps-3 material-symbols-outlined text-gray-400"
+          >
+            close
+          </span>
+        </div>
+      )}
     </div>
   );
 }
