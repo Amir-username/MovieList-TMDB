797693211afbda8a7dab4bf8478190e7b98a7700
Amir <najiamirdesktop@gmail.com>
2025-01-25 16:18:18 +0330

    update image loading

--- src/components/loading/ImageLoading.tsx
+++ src/components/loading/ImageLoading.tsx
@@ -4,8 +4,8 @@ type ImageLoadingProps = {
 
 function ImageLoading({type}: ImageLoadingProps) {
   return (
-    <div className="flex items-center justify-center w-28 h-[10.5rem] rounded-lg bg-gray-300 dark:bg-gray-700">
-      <span className="material-symbols-outlined text-6xl text-gray-400 dark:text-gray-500">
+    <div className="flex items-center justify-center bg-gray-300 rounded-lg w-44 h-72 dark:bg-gray-700">
+      <span className="text-6xl text-gray-400 material-symbols-outlined dark:text-gray-500">
         {type}
       </span>
     </div>
